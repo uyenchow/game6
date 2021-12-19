a60f22ed0eb2db2ac1e46b98a01d66b6818a5849
uyenchow <65537851+uyenchow@users.noreply.github.com>
2021-12-19 17:27:33 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Sequential Timing Tool for Animations
+
+// Change $delay-base to change the timing.
+
+// by Bruce Vang @brucevang
+
+// TODO: Add jQuery for timing
+// http://demosthenes.info/blog/630/Fade-In-Elements-Sequentially-With-JQuery-and-CSS3
